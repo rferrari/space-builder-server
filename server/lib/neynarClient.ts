08c4aa80bd245e96ab6d6debc2f0cc451189116d
Ricardo Ferrari <rferrariwd@gmail.com>
2024-12-11 17:45:30 -0300

    feat: x-neynar-experimental

--- server/lib/neynarClient.ts
+++ server/lib/neynarClient.ts
@@ -1,6 +1,16 @@
 import { NeynarAPIClient } from "@neynar/nodejs-sdk";
 import { NEYNAR_API_KEY } from "../config";
 
-const neynarClient = new NeynarAPIClient(NEYNAR_API_KEY);
+const neynarConfig = {
+    baseOptions: {
+      headers: {
+        "x-neynar-experimental": true,
+      },
+    },
+  };
+  
+//   const client = new NeynarAPIClient(config);
+
+const neynarClient = new NeynarAPIClient(NEYNAR_API_KEY, neynarConfig);
 
 export default neynarClient;
